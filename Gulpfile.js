dd424bbfb59a6ad974068dc92755540b187fba6d
Jarek Pelczynski <jpelczynski@gmail.com>
2017-02-22 22:28:14 +0100

    Setup gulp to wath sass files

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,6 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
-var concat      = require('gulp-concat-css');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -10,17 +9,16 @@ gulp.task('serve', ['sass'], function() {
         server: "./app"
     });
 
-    gulp.watch("app/sass/config.scss", ['sass']);
+    gulp.watch("app/sass/**/*.scss", ['sass']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("app/sass/config.scss")
+    return gulp.src("app/sass/styles.scss")
         .pipe(sass({
           includePaths: require('node-normalize-scss').includePaths
         }))
-        .pipe(concat('app.css'))
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
 });
