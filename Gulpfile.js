ea1cc20ab70ba579a3fae3fa034ea2e65d1db6ae
Jarek Pelczynski <jpelczynski@gmail.com>
2017-02-23 10:08:25 +0100

    Add autoprefixer to support legacy browsers

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,8 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-var concat      = require('gulp-concat');
+var gulp         = require('gulp');
+var browserSync  = require('browser-sync').create();
+var sass         = require('gulp-sass');
+var concat       = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -20,6 +21,10 @@ gulp.task('sass', function() {
     return gulp.src("app/sass/styles.scss")
         .pipe(sass({
           includePaths: require('node-normalize-scss').includePaths
+        }))
+        .pipe(autoprefixer({
+          browsers: ['last 2 versions', '> 5%'],
+          cascade: false
         }))
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
